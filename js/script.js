124506aa2ea7e522b5d506dd298dbfefc162e31f
Daniel J. Garcia-Carmona <danny.garcia80@hotmail.com>
2020-03-09 02:15:50 -0700

    will finish project 1

--- js/script.js
+++ js/script.js
@@ -8,45 +8,45 @@ project 1 - A Random Quote Generator
   // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /*** 
- * `quotes` array 
+ * `quotes` array section
 ***/
 
 // Will create the array of quote objects and name it quotes
 var quotes = [
   {
-    //Will add quote 1
+    //Will add quote and author as strings
     quote: "Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do.",
     author: "H.Jackson Brown Jr"
   },
   {
-    //Will add quote 2
+    
     quote: "Every human life is worth the same, and worth saving.",
     author: "J.K. Rowling"
   },
   {
-    //Will add quote 3
+    
     quote: "Get busy living, or get busy dying.",
     author: "Stephen King"
   },
   {
-    //Will add quote 4
+    
     quote: "The goal isn’t to live forever, the goal is to create something that will.",
     author: "Chuck Palahniuk"
   },
   {
-    //Will add quote 5
+    
     quote: "Travel far enough, you meet yourself.",
     author: "David Mitchell"
   },
   {
-    //Will add quote 6
+    
     quote: "None of us really changes over time. We only become more fully what we are.",
     author: "Anne Rice"
   }
 ];
 
 /***
- * `getRandomQuote` function
+ * `getRandomQuote` function section
 ***/
 
 // Will create the getRandomQuote function and name it getRandomQuote
@@ -55,7 +55,7 @@ function getRandomQuote(arr){
 }
 
 /***
- * `printQuote` function
+ * `printQuote` function section
 ***/
 
 // Will create the printQuote function and name it printQuote
@@ -67,7 +67,7 @@ function printQuote(){
 }
 
 /***
- * click event listener for the print quote button
+ * click event listener for the print quote button section
  * DO NOT CHANGE THE CODE BELOW!!
 ***/
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Random Quote Generator
+
+Simple Random Quote Generator that outputs random quote from a given array.
